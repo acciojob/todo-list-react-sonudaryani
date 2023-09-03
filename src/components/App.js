7d8242c045c8f03c76dbddb4f12e17bf006ec23e
= <=>
2023-09-03 13:42:21 +0530

    update v1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,65 @@
+import "./styles.css";
+import React, { useState } from "react";
 
-import React from "react";
-import './../styles/App.css';
+export default function App() {
+  let [todoList, setTodoList] = useState([]);
+  let [todo, setTodo] = useState("");
 
-const App = () => {
+  const handleClick = () => {
+    setTodoList([...todoList, todo]);
+    setTodo("");
+  };
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="add_tasks_section">
+      <input onChange={(e) => setTodo(e.target.value)} />
+      <button onClick={handleClick}>Add</button>
+      <diV className="tasks_section">
+      {todoList.map((todo, index) => (
+        <InputChild
+          key={index}
+          todo={todo}
+          todoList={todoList}
+          setTodoList={setTodoList}
+        />
+      ))}
+      </diV>
     </div>
-  )
+  );
 }
 
-export default App
+const InputChild = ({ todo, todoList, setTodoList }) => {
+  let [isEditBtnClicked, setIsEditBtnClicked] = useState(false);
+  let [updatedTodo, setUpdatedTodo] = useState("");
+
+  const handleDeleteBtnClick = () => {
+    const newTodoList = todoList.filter((item) => item != todo);
+    console.log(newTodoList);
+    setTodoList(newTodoList);
+  };
+
+  const handleUpdateBtn = () => {
+    const updatedTodoList = todoList.map((item) => {
+      if (item === todo) {
+        return updatedTodo;
+      } else return item;
+    });
+    setTodoList(updatedTodoList);
+    setIsEditBtnClicked(false);
+  };
+  return (
+    <div className="task">
+      {isEditBtnClicked ? (
+        <div>
+          <input onChange={(e) => setUpdatedTodo(e.target.value)} />
+          <button className="save" onClick={handleUpdateBtn}>Save</button>
+        </div>
+      ) : (
+        <div>
+          <p>{todo}</p>
+          <button className="edit" onClick={() => setIsEditBtnClicked(true)}>Edit</button>
+          <button className="delete" onClick={handleDeleteBtnClick}>Delete</button>
+        </div>
+      )}
+    </div>
+  );
+};
